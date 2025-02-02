e10950c5130d2845d7ba58749b349fc3418c6e44
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-02-02 16:03:28 -0300

    Update CalculadoraDeTempo.java

--- src/br/com/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,4 +1,15 @@
 package br.com.screenmatch.calculos;
 
+import br.com.screenmatch.modelos.Titulo;
+
 public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal(){
+        return tempoTotal;
+    }
+
+    public void inclui(Titulo titulo) {
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
 }
